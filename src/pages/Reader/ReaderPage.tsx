178a845609758faffc73ec2bf6797f69567d0f4b
Umar Jamil <hkproj@gmail.com>
2023-11-01 08:48:27 +0800

    added results viewer

--- src/pages/Reader/ReaderPage.tsx
+++ src/pages/Reader/ReaderPage.tsx
@@ -1,19 +1,36 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap'
-import { GetSelectedTextRequest, GetSelectedTextResponse, RequestType, SegmentTextRequest, SegmentTextResponse } from '../../shared/messages'
+import { DUMMY_CONTENT, GetSelectedTextRequest, GetSelectedTextResponse, RequestType, SearchTermRequest, SearchTermResponse, SegmentTextRequest, SegmentTextResponse } from '../../shared/messages'
 import { ResourceLoadStatus } from '../../shared/loading';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './ReaderPage.scss';
+import { ResultsViewer } from './ResultsViewer';
+import { getWordUnderCursor } from '../Content/textSelect';
 
-import '../Content/index'
-import '../Content/content.styles.css'
+const EMPTY_RESULTS: SearchTermResponse = {
+  dictionary: {
+    data: [],
+    maxMatchLen: 0
+  },
+  hsk: [],
+  knownWords: [],
+  serviceEnabled: true,
+  status: ResourceLoadStatus.Loaded,
+  dummy: DUMMY_CONTENT
+}
 
 const Reader = () => {
 
   const [text, setText] = useState('');
   const [segmentsLoadingStatus, setSegmentsLoadingStatus] = useState(ResourceLoadStatus.Unloaded);
   const [textSegments, setTextSegments] = useState<string[]>([]);
+
+  const [selectedSegmentNode, setSelectedSegmentNode] = useState<Node | null>(null);
+  const [selectedSegmentOffset, setSelectedSegmentOffset] = useState<number | null>(null);
+  const [selectedSegmentIndex, setSelectedSegmentIndex] = useState<number | null>(null);
+
+  const [searchTermResponse, setSearchTermResponse] = useState<SearchTermResponse>(EMPTY_RESULTS);
 
   useEffect(() => {
     const request: GetSelectedTextRequest = { type: RequestType.GetSelectedText, clean: true }
@@ -36,15 +53,120 @@ const Reader = () => {
 
   }, [text])
 
+  function onKeyUpDocument(event: KeyboardEvent) {
+    if (event.code == 'KeyQ') {
+      // Split the current segment into two segments based on the cursor position
+      if (selectedSegmentNode != null && selectedSegmentOffset != null) {
+        const segmentText = textSegments[selectedSegmentIndex!]
+
+        if (segmentText.length <= 1) {
+          // Nothing to split
+          return
+        }
+
+        const firstSegmentText = segmentText.substring(0, selectedSegmentOffset)
+        if (firstSegmentText.length == 0) {
+          // Nothing to split
+          return
+        }
+
+        const secondSegmentText = segmentText.substring(selectedSegmentOffset)
+
+        const newSegments = [...textSegments]
+        newSegments.splice(selectedSegmentIndex!, 1, firstSegmentText, secondSegmentText)
+        setTextSegments(newSegments)
+      }
+    } else if (event.code == 'KeyW') {
+      // Join the selected segments together
+      const selection = window.getSelection()
+      if (selection == null) {
+        return
+      }
+      const range = selection.getRangeAt(0)
+      const startNode = range.startContainer
+      const endNode = range.endContainer
+      if (startNode == null || endNode == null) {
+        return
+      }
+
+      const startSegmentIndex = parseInt(startNode.parentElement?.getAttribute('data-segment-index')!)
+      const endSegmentIndex = parseInt(endNode.parentElement?.getAttribute('data-segment-index')!)
+      if (startSegmentIndex == null || endSegmentIndex == null || Number.isNaN(startSegmentIndex) || Number.isNaN(endSegmentIndex) || startSegmentIndex == endSegmentIndex) {
+        return
+      }
+
+      // Only allow to join up to three segments
+      if (endSegmentIndex - startSegmentIndex > 2) {
+        return
+      }
+
+      const newSegments = [...textSegments]
+      let joinedSegmentText = ""
+      for (let i = startSegmentIndex; i <= endSegmentIndex; i++) {
+        joinedSegmentText += newSegments[i]
+      }
+
+      if (joinedSegmentText.length == 0) {
+        return
+      }
+
+      newSegments.splice(startSegmentIndex, endSegmentIndex - startSegmentIndex + 1, joinedSegmentText)
+      setTextSegments(newSegments)
+    }
+  }
+
+  function onMouseMoveSegmentList(event: React.MouseEvent<HTMLDivElement>) {
+    const range = document.caretRangeFromPoint(event.clientX, event.clientY)
+    if (range == null || range.startContainer == null || range.startOffset == null) {
+      return;
+    }
+
+    const node = range!.startContainer
+    // Check if the node has an attribute data-segment-index
+    const segmentIndex = node.parentElement?.getAttribute('data-segment-index')
+    if (segmentIndex != null) {
+      setSelectedSegmentNode(node)
+      setSelectedSegmentOffset(range!.startOffset)
+      setSelectedSegmentIndex(parseInt(segmentIndex))
+    }
+  }
+
+  function onMouseMoveDocument(event: MouseEvent) {
+    // Search the dictionary
+    const [wordUnderCursor, nodeUnderCursor] = getWordUnderCursor(event)
+    if (wordUnderCursor == "" || nodeUnderCursor == null) {
+      return
+    }
+
+    // console.log(`Word under cursor: ${wordUnderCursor} - Node: ${node}`)
+    const request: SearchTermRequest = { type: RequestType.SearchTerm, searchTerm: wordUnderCursor }
+
+    chrome.runtime.sendMessage(request)
+      .then((response: SearchTermResponse) => {
+        if (response != null) {
+          setSearchTermResponse(response)
+        }
+      })
+    }
+
+  useEffect(() => {
+    document.addEventListener("keyup", onKeyUpDocument);
+    document.addEventListener("mousemove", onMouseMoveDocument);
+    return () => {
+      document.removeEventListener("keyup", onKeyUpDocument);
+      document.removeEventListener("mousemove", onMouseMoveDocument);
+    };
+  }, [onKeyUpDocument, onMouseMoveDocument]);
+
   function getTextView() {
     if (text == null) {
       return <p>No text selected.</p>;
     } else if (segmentsLoadingStatus === ResourceLoadStatus.Loading) {
       return <p>Loading...</p>;
     } else {
-      return <div className="segment-list">
+      return <div className="segment-list" onMouseMove={onMouseMoveSegmentList}>
         {textSegments.map((segment, index) => {
-          return <span className="segment" key={index}>{segment}</span>
+          return <span data-segment-index={index} className="segment" key={index}>{segment}</span>
         })}
       </div>
     }
@@ -60,7 +182,7 @@ const Reader = () => {
           {getTextView()}
         </div>
         <div className="col-4">
-          <div id="hanbaobao-window"></div>
+          <ResultsViewer response={searchTermResponse} />
         </div>
       </div>
     </Container>
